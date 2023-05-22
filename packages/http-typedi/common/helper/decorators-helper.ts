21959aaac094e37358f7a9e72469cd34776f7acb
Jack <84073969+fkc-alt@users.noreply.github.com>
2023-05-22 18:34:22 +0800

    Update decorators-helper.ts

--- packages/http-typedi/common/helper/decorators-helper.ts
+++ packages/http-typedi/common/helper/decorators-helper.ts
@@ -5,6 +5,7 @@ export const createDecoratorBind = (
   params: any
 ): MethodDecorator & ClassDecorator => {
   return function (...args: any) {
+    const [target, propertyKey = ''] = args as Parameters<MethodDecorator>
     const _propertyKey = [propertyKey].filter(Boolean)
     const metadataArgs: any = [key, params, target, ..._propertyKey]
     Reflect.defineMetadata.apply(null, metadataArgs)
