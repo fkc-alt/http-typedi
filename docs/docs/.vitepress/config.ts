1595c4540c87b28ec4921fd2c83810f4a2e5ba0f
Jack <fengkaichao149@163.com>
2023-05-23 23:17:03 +0800

    chore: create vitepress

---
+++ docs/docs/.vitepress/config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    // site-level options
+    title: 'VitePress',
+    description: 'Just playing around.',
+    base: './',
+    themeConfig: {
+        // theme-level options
+    }
+})
