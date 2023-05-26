b45faf0415f92c850bfed65ded789de84e957018
Jack <fengkaichao149@163.com>
2023-05-27 01:02:39 +0800

    fix: build docs not nprogress

--- docs/docs/.vitepress/theme/index.ts
+++ docs/docs/.vitepress/theme/index.ts
@@ -1,22 +1,24 @@
 import DefaultTheme from 'vitepress/theme'
-import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import './styles/index.css'
 import type { Theme } from 'vitepress'
-
-NProgress.configure({ showSpinner: false })
 
 const define = <T>(value: T): T => value
 export default define<Theme>({
   ...DefaultTheme, // 此处采用了默认主题，可以替换为自定义的主题
   NotFound: DefaultTheme.NotFound,
   enhanceApp: ({ app, router, siteData }) => {
-    router.onAfterRouteChanged = (to) => {
-      NProgress.done();
-    }
-    router.onBeforeRouteChange = (to) => {
-      NProgress.start();
-    }
+    import('nprogress').then((m) => {
+      // app.use(m.default);
+      const NProgress = m.default
+      NProgress.configure({ showSpinner: false })
+      router.onAfterRouteChanged = (to) => {
+        NProgress.done();
+      }
+      router.onBeforeRouteChange = (to) => {
+        NProgress.start();
+      }
+    });
     // console.log(siteData)
   },
 })
