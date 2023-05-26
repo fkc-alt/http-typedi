08d3de1734f64c148eb1209d42a318253f138633
Jack <fengkaichao149@163.com>
2023-05-27 00:49:16 +0800

    feat: docs nprogress

--- docs/docs/.vitepress/theme/index.ts
+++ docs/docs/.vitepress/theme/index.ts
@@ -1,13 +1,22 @@
 import DefaultTheme from 'vitepress/theme'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import './styles/index.css'
 import type { Theme } from 'vitepress'
+
+NProgress.configure({ showSpinner: false })
 
 const define = <T>(value: T): T => value
 export default define<Theme>({
   ...DefaultTheme, // 此处采用了默认主题，可以替换为自定义的主题
   NotFound: DefaultTheme.NotFound,
   enhanceApp: ({ app, router, siteData }) => {
-    // console.log(router);
+    router.onAfterRouteChanged = (to) => {
+      NProgress.done();
+    }
+    router.onBeforeRouteChange = (to) => {
+      NProgress.start();
+    }
     // console.log(siteData)
   },
 })
