78c227856d84b933f601dd377f749bcbef71b4f7
Jack <fengkaichao149@163.com>
2023-06-02 00:52:38 +0800

    fix: ci/cd

--- docs/docs/vite.config.ts
+++ docs/docs/vite.config.ts
@@ -1,10 +1,3 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './.vitepress/')
-    }
-  }
-})
+export default defineConfig({})
